d0e79defbbb42e79a8bcd113efccdcaa9e4424fe
Peppssi <jesper2follin@gmail.com>
2026-03-06 11:19:47 +0100

    initial nextjs + flask setup

---
+++ api/neural_net.py
@@ -0,0 +1,6 @@
+
+def run_model():
+    # Simulate running a neural network model
+    import time
+    time.sleep(5)  # Simulate time taken to run the model
+    return "Model run successfully"
---
+++ api/run.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from neural_net import run_model
+import time
+
+app = Flask(__name__)
+
+@app.route("/api/run")
+def run_script():
+    # simulate long running python script
+    time.sleep(2)
+
+    return jsonify({
+        "message": "Python API working"
+    })
