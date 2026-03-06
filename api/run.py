17cd46a7039cdb65c5b1e2d5fbe96f35000265f6
Peppssi <jesper2follin@gmail.com>
2026-03-06 14:38:01 +0100

    FEAT: Prepped for vercel deployment

--- api/run.py
+++ api/run.py
@@ -1,21 +1,17 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from NN import run_model
+from .NN import run_model
 
 app = Flask(__name__)
+
+# this should be deletable in prod
 CORS(app)
 
 @app.route("/api/run")
 def run_script():
-    
     accuracy = run_model()
 
     return jsonify({
         "status": "finished",
         "accuracy": accuracy
     })
-
-
-# for testing only
-if __name__ == "__main__":
-    app.run(port=8080)
