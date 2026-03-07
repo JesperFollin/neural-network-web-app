7f371605db3d97754477a81d1d6e829132bc70c0
Peppssi <jesper2follin@gmail.com>
2026-03-07 14:44:44 +0100

    feat: Updated run.py to accept time elapsed from NN.py

--- api/run.py
+++ api/run.py
@@ -1,17 +1,17 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from .NN import run_model
+from NN import run_model
 
 app = Flask(__name__)
-
-# this should be deletable in prod
 CORS(app)
 
 @app.route("/api/run")
 def run_script():
-    accuracy = run_model()
+    
+    accuracy, timeElapsed = run_model()
 
     return jsonify({
         "status": "finished",
-        "accuracy": accuracy
+        "accuracy": accuracy,
+        "timeElapsed": timeElapsed
     })
