a14009c1d1f51aedb6962b860e6453f5ffab5343
Peppssi <jesper2follin@gmail.com>
2026-03-09 11:32:06 +0100

    FIX: Logo no display bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,11 +17,7 @@ export const metadata: Metadata = {
   title: "Learn Neural Networks",
   description: "Learn how neural networks work with and interactive demo and explanations in code.",
   icons: {
-    icon: [
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    ],
-    apple: "/apple-touch-icon.png", // 180x180 for iOS
+    icon: "/logo.png",
   },
 };
 
